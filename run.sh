2cb8cf6ad218c8d8c0ef351efc05bd9a6961b7e4
syjn99 <syjn99@gmail.com>
2025-01-12 14:58:55 +0900

    Add boot code for kernel

--- run.sh
+++ run.sh
@@ -4,5 +4,14 @@ set -xue
 # QEMU file path
 QEMU=/opt/homebrew/bin/qemu-system-riscv32
 
+# Path to clang and compiler flags
+CC=/opt/homebrew/opt/llvm/bin/clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# Build the kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
+
 # Start QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf # Load the kernel
